3bcf814f99c5bcfa798fd46003a8059a6a080345
AndreyLipato <andrey.lipatov.19@mail.ru>
2021-12-20 22:29:52 +0300

    main start

--- src/main.py
+++ src/main.py
@@ -1,9 +1,22 @@
+import pygame
+
 class Main:
     def __init__(self):
-        print('test')
+        size = width, height = 600, 600
+        self.screen = pygame.display.set_mode(size)
+
+        self.running = True
 
     def start(self):
-        pass
+        while self.running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+            self.screen.fill((0, 0, 0))
+            pygame.display.flip()
+            self.screen.fill((0, 0, 0))
+        pygame.quit()
+
 
 
 if (__name__ == "__main__"):
