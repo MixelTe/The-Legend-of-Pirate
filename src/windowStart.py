c5974e4a9b2181da411dcd6be9063715c68dbcae
AndreyLipato <andrey.lipatov.19@mail.ru>
2021-12-24 00:11:07 +0300

    Создание кнопки quit

--- src/windowStart.py
+++ src/windowStart.py
@@ -4,12 +4,16 @@ from functions import load_image
 
 
 class WindowStart(Window):
-    image = load_image("start.png", -1)
+    image_start = load_image("start.png", -1)
+    image_quit = load_image("quit.png", -1)
 
     def __init__(self):
         scale = 12
-        self.rect = pygame.Rect(500, 500, WindowStart.image.get_width() * scale, WindowStart.image.get_height() * scale)
-        self.image = pygame.transform.scale(self.image, (self.rect.width, self.rect.height))
+        self.rect_start = pygame.Rect(750, 200, WindowStart.image_start.get_width() * scale, WindowStart.image_start.get_height() * scale)
+        self.image_start = pygame.transform.scale(self.image_start, (self.rect_start.width, self.rect_start.height))
+        
+        self.rect_quit = pygame.Rect(750, 400, WindowStart.image_quit.get_width() * scale, WindowStart.image_quit.get_height() * scale)
+        self.image_quit = pygame.transform.scale(self.image_quit, (self.rect_quit.width, self.rect_quit.height))
 
     def draw(self, screen: pygame.Surface):
-        screen.blit(self.image, self.rect)
+        screen.blit(self.image_start, self.rect_start)
