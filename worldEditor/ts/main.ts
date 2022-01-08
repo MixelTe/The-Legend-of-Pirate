ff329f166b414b4d6db18b9d187193ced0961c37
Mike <63655075+MixelTe@users.noreply.github.com>
2022-01-08 12:51:23 +0300

    worldEditor: tiles moved

--- worldEditor/ts/main.ts
+++ worldEditor/ts/main.ts
@@ -25,24 +25,7 @@ const ViewWidth = 15;
 const ViewHeight = 7;
 let TileSize = 16 * 2;
 // let TileSize = 16 * 4; // Temp
-const tileIds = {
-	sand1: "sand1.png",
-	sand2: "sand2.png",
-	sand3: "sand3.png",
-	grass1: "grass1.png",
-	grass2: "grass2.png",
-	grass3: "grass3.png",
-	water_deep: "water_deep.png",
-	water_low: "water_low.png",
-	water_sand: "water_sand.png",
-	mountain: "mountain.png",
-	A: "A.png",
-	AttackB: "AttackB.png",
-	D: "D.png",
-	DigB: "DigB.png",
-	S: "S.png",
-	W: "W.png",
-}
+
 const entity: (typeof Entity)[] = [];
 for (const key in EntityDict) entity.push(EntityDict[<keyof typeof EntityDict>key])
 const tileImages: TileImages = {};
---
+++ worldEditor/ts/tiles.ts
@@ -0,0 +1,18 @@
+const tileIds = {
+	sand1: "sand1.png",
+	sand2: "sand2.png",
+	sand3: "sand3.png",
+	grass1: "grass1.png",
+	grass2: "grass2.png",
+	grass3: "grass3.png",
+	water_deep: "water_deep.png",
+	water_low: "water_low.png",
+	water_sand: "water_sand.png",
+	mountain: "mountain.png",
+	A: "A.png",
+	AttackB: "AttackB.png",
+	D: "D.png",
+	DigB: "DigB.png",
+	S: "S.png",
+	W: "W.png",
+}
