3504e753d5b3044415f1d434dfed056508d484e3
Mike <63655075+MixelTe@users.noreply.github.com>
2022-01-08 23:08:54 +0300

    Animator blank

--- src/game/animator.py
+++ src/game/animator.py
@@ -1,15 +1,38 @@
 import pygame
-from typing import Union
+
+
+class AnimatorData:
+    def init(self, folder: str, animations: list[tuple[str, int, tuple[int, int], tuple[int, int, int, int]]]):
+        self.frames: dict[str, tuple[list[pygame.Surface], int, tuple[int, int]]] = {}
+        # все кадры анимации, скорость переключения кадров (милисекунды между кадрами) и позиция картинки относительно сущности для каждой анимации
+
+        # folder - папка с анимациями
+        # animations - список tuple с содержимом:
+            # [0] - название файла с анимацией
+            # [1] - промежуток между кадрами (скорость переключения)
+            # [2] - размер кадра в анимации
+            # [3] - положение относительно сущности и требуемый размер кадра
+        # Добавляет каждую анимацию в frames, разделив на кадры и применив масштаб imgSize. Ключ - название файла без расширения
+        # Для каждой анимации в animations:
+            # Делит картинку [0] на кадры размером [2]
+            # Применяет масштаб [3] для каждого кадра
+            # В frames[название_файла_без_расширения] кладёт tuple, с содержимым:
+                # Список со всеми увеличеными кадрами
+                # Промежуток между кадрами [1]
+                # Положение кадра относительно сущности [3]
+
+    def get_image(self, animation: str, index: int):
+        return (self.frames[animation][0][index], self.frames[animation][2])
+
+    def get_speed(self, animation: str):
+        return self.frames[animation][1]
 
 
 class Animator:
-    def init(self, image: pygame.Surface, frameSize: tuple[int, int], animation: list[int, int]):
-        self.image = image # картинка с анимациями, каждая анимация на новой строке
-        self.frameSize = frameSize
-        self.animation = animation # tuple[скорость переключения кадров, кол-во кадров] для каждой анимации.
-        self.frame = (0, 0) # tuple[строка, картинка]
+    def init(self, data: AnimatorData, frame: tuple[str, int]):
+        self.data = data # все анимации и их кадры
+        self.frame = frame # tuple[анимация, картинка] текущий кадр
         self.counter = 0 # счетчик для переключения кадров с определённой скоростью
-        self.names: list[str] = [] # названия анимаций
 
     def update(self) -> tuple[bool, bool]:
         # прибавляет счётчик, и переключает кадр, если прошло достаточно времени. После последнего кадра идёт первый.
@@ -18,14 +41,14 @@ class Animator:
         # Поледний ли это кадр анимации
         pass
 
-    def getImage(self) -> pygame.Surface:
-        # возвращает текущий кадр
+    def getImage(self) -> tuple[pygame.Surface, tuple[int, int]]:
+        # возвращает текущий кадр и его позицию относительно сущности
         pass
 
-    def setAnimation(self, animation: Union[int, str]):
-        # устанавливает текущую анимацию по номеру или её названию
+    def setAnimation(self, animation: str):
+        # устанавливает текущую анимацию по её названию
         pass
 
-    def curAnimation(self) -> tuple[int, Union[str, None]]:
-        # номер и название (если есть, иначе None) текущей анимации
+    def curAnimation(self) -> str:
+        # название текущей анимации
         pass
