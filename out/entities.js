ab79120f624bdf6e87adf7dea09de0d089174e15
Mike <63655075+MixelTe@users.noreply.github.com>
2022-01-11 17:40:12 +0300

    update

--- out/entities.js
+++ out/entities.js
@@ -13,6 +13,6 @@ createNewEntityClass_Auto("crab", false, 20, 11, 1, 0.55);
 createNewEntityClass_Auto("cactus", false, 16, 16, 1, 1);
 createNewEntityClass_Auto("door", false, 11, 12, 1, 1);
 createNewEntityClass_Auto("palm", false, 20, 22, 0.45, 0.7, -0.45, -0.8, 1.5, 1.5);
-createNewEntityClass_Auto("trader", true, 14, 24, 0.75, 1, 0, -0.5, 0.75, 1.5);
+createNewEntityClass_Auto("trader", true, 14, 24, 0.75, 0.7, 0, -0.8, 0.75, 1.5);
 createNewEntityClass_Auto("cannon", true, 11, 12, 1, 1);
-createNewEntityClass_Auto("trainer", false, 9, 18, 0.75, 1, 0, -0.5, 0.75, 1.5);
+createNewEntityClass_Auto("trainer", false, 9, 18, 0.75, 0.7, 0, -0.8, 0.75, 1.5);
